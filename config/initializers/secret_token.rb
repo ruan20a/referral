182e24d5e5a3657aa6dea7c32eaf48950839b0c4
loritiernan <loritiernan@gmail.com>
2014-04-04 14:45:50 -0400

    basic devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wekrut::Application.config.secret_key_base = '855c6c5cad51a7515ba4352e0ba92bae5347bf64bd8631cdec07b177725d2b634a6def8d738aefa422711c07a1f49b275b5a640ecb81eb6efcdf1d303355b5bc'
