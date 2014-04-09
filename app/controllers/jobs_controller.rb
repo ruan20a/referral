c0ce1217469cfa53196e385a26b126a2f44eca35
loritiernan <loritiernan@gmail.com>
2014-04-08 21:56:18 -0400

    job postings created

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,23 @@
 class JobsController < ApplicationController
 	def index
+	@jobs = Job.all
 	end
+	def new
+	@job = Job.find_by_id(params[:id])
+	end
+	def show
+    @job = Job.find_by_id(params[:id])
+    @job = Job.new
+  	end
+	def create
+  	@job = Job.new(job_params)
+ 
+  	@job.save
+  	redirect_to @job
+	end
+ 
+	private
+  	def job_params
+    params.require(:job).permit(:name, :description, :recuiter_id, :referral_fee)
+  	end
 end
