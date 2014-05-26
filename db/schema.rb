77ee08608e9324b21d6f386a08fd73602aee2293
Amy Ruan <nyc.amy@gmail.com>
2014-05-26 14:28:21 -0400

    changed description column type to enable long descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520211026) do
+ActiveRecord::Schema.define(version: 20140526182616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 20140520211026) do
 
   create_table "jobs", force: true do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.integer  "recruiter_id"
     t.string   "speciality_1"
     t.string   "speciality_2"
