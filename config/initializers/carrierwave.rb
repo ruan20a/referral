57297b787a7420cffb4dcdea65f806b226a8903e
loritiernan <loritiernan@gmail.com>
2014-05-31 15:45:31 -0400

    new access codes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['S3_KEY'],                       # required
-    :aws_secret_access_key  => ENV['S3_SECRET'],
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                       # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     :region                 => 'us-east-1'                        # required
   }
-  config.fog_directory  = ENV['S3_BUCKET']                     # required
+  config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 config.storage = :fog
