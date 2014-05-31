99cb5870bac9b1e8295cf7d62780ebfb79e2b47a
loritiernan <loritiernan@gmail.com>
2014-05-31 16:43:48 -0400

    heroku issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Wekrut::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
    config.action_mailer.default_url_options = { host: 'localhost:3000', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     :region                 => 'us-east-1'                        # required
   }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
+  config.fog_directory  = ENV['AWS_BUCKET_NAME']                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 config.storage = :fog
