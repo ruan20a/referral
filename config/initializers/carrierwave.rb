e37078a265f8b9c55efa86a1742d7b57626535c5
loritiernan <loritiernan@gmail.com>
2014-05-31 13:00:36 -0400

    removed fog file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['S3_KEY'],                       # required
-    :aws_secret_access_key  => ENV['S3_SECRET']                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET'],
+    :region                 => 'us-east-1'                        # required
   }
   config.fog_directory  = ENV['S3_BUCKET']                     # required
   config.fog_public     = false                                   # optional, defaults to true
