b9494aa20e77e8835fa876057db2725d1c44f03f
deaglanm <deaglan1@gmail.com>
2014-05-31 20:24:19 -0400

    all css

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,10 @@
 
 Whitelist.create(email: "loritiernan@gmail.com", is_admin: false)
 Whitelist.create(email: "info@wekrut.com", is_admin: true)
+
+
+
+Referral.create(name: "Lori Tiernan", referral_name: "Deaglan McEachern", relationship: "friend", referral_email: "deaglan1@gmail.com", additional_details: "Deaglan is awesome", linked_profile_url: "http://www.linkedin.com/profile/view?id=62134001&locale=en_US&trk=tyah&trkInfo=tarId%3A1397934136810%2Ctas%3Adea%2Cidx%3A2-1-2", status: "Interview Stage", job_id: 8, admin_id: 1 )
+
+Referral.create(name: "Deaglan McEachern", referral_name: "Lori Tiernan", relationship: "coworker", referral_email: "loritiernan@gmail.com", additional_details: "Lori is the best", linked_profile_url: "http://www.linkedin.com/profile/view?id=19416736&trk=nav_responsive_tab_profile", status: "Interview Stage", job_id: 8, admin_id: 1 )
+
