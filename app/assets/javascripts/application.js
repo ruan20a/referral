fdf7fe69d6ac0d15e6b2c0fe0545a4cbb5de31a7
deaglanm <deaglan1@gmail.com>
2014-06-01 16:49:30 -0400

    more css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require main
-//= require_tree .
+//= require_tree
--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -25,7 +25,7 @@ class ReferralsController < ApplicationController
  end
 
   def create
-    binding.pry
+    #binding.pry
     @referral = Referral.new(referral_params)
     @admin = @referral.job.admin
 
