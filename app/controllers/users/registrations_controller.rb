4496f1a777ddd7ac211565c0c903108c231e3646
loritiernan <loritiernan@gmail.com>
2014-06-17 09:54:33 -0400

    changes to registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,27 +4,31 @@ include UsersHelper
 
   def create
     email = params[:user][:email].downcase
-    inviter_email = params[:user][:inviter_email].downcase
-    inviter_id = User.find_by_email(inviter_email)
 
     unless Whitelist.exists?(:email => email)
       flash[:alert] = "#{email} is currently not on our beta list yet. Sign up #{view_context.link_to('here', root_path)} for our beta version.".html_safe
       redirect_to new_user_registration_path
     else
       resource = build_resource(sign_up_params)
-      # binding.pry
       if resource.save
         yield resource if block_given?
         # if resource.active_for_authentication?
-        if User.exists?(:email => inviter_email)
-          if Invitation.exists?(:user_id => inviter_id, :invited_email => email)
-            invitation = Invitation.where(:user_id => inviter_id, :invited_email => email)[0]
-            invitation.record_success
-            InvitationMailer.send_successful_invitation(invitation)
+        # end
+        if !params[:user][:inviter_email].blank?
+          if User.exists?(:email => params[:user][:inviter_email].downcase)
+            inviter_email = params[:user][:inviter_email].downcase
+            inviter_id = User.find_by_email(inviter_email).id
+            if Invitation.exists?(:user_id => inviter_id, :invited_email => email)
+              invitation = Invitation.where(:user_id => inviter_id, :invited_email => email)[0]
+              invitation.record_success
+              InvitationMailer.send_successful_invitation(invitation)
+            end
+            set_flash_message :notice, :signed_up if is_flashing_format? #flash for successful inviter
+          else
+            flash[:notice] = "Account successfully created. Please note that we could not find the inviter email in our user database." #flash for bad inviter
           end
-          set_flash_message :notice, :signed_up if is_flashing_format?
         else
-          flash[:notice] = "Account successfully created. Please note that we could not find the inviter email in our user database."
+          set_flash_message :notice, :signed_up if is_flashing_format? #flash for no inviter
         end
         sign_up(resource_name, resource)
         redirect_to jobs_path
@@ -38,10 +42,6 @@ include UsersHelper
         respond_with resource
       end
     end
-    # unless Whitelist.exists?(:email => params[:user][:email])
-    # else
-    #   super
-    # end
   end
 
 end
