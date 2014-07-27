c01637a71787f8aa7508cdd7b2f3d5c23f9614d2
loritiernan <loritiernan@gmail.com>
2014-07-26 20:42:41 -0400

    add user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726234158) do
+ActiveRecord::Schema.define(version: 20140727004142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -195,6 +195,7 @@ ActiveRecord::Schema.define(version: 20140726234158) do
     t.hstore   "educations"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "user_whitelists", force: true do |t|
