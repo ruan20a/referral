80c0a6b823a484eec882840835f1e588b5ae17d1
loritiernan <loritiernan@gmail.com>
2014-10-08 22:29:40 -0400

    front end jobs index and show

--- app/assets/javascripts/grayscale.js
+++ app/assets/javascripts/grayscale.js
@@ -16,6 +16,14 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
+
+// Focus state for append/prepend inputs
+   $('.input-group').on('focus', '.form-control', function () {
+     $(this).closest('.input-group, .form-group').addClass('focus');
+   }).on('blur', '.form-control', function () {
+     $(this).closest('.input-group, .form-group').removeClass('focus');
+   });
 });
 
 jQuery('.data-table').each(function() {
